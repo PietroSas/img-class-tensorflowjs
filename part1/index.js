5e3cd26dff61563b14db202f857e32f884f8c102
Pietro <pietro.sales@gmail.com>
2019-08-31 20:20:15 -0300

    Part 1 - Upload file

--- part1/index.js
+++ part1/index.js
@@ -1,16 +1,28 @@
-let net;
+const img_output = document.getElementById('img-output');
+
+function loadFile(event) {
+  img_output.src = URL.createObjectURL(event.target.files[0]);
+}
+
+async function classifyImg() {
+  console.log("Classify in progress...");
+  const result = await net.classify(img_output);
+  console.log("Classification completed");
+  document.getElementById('answer').innerText = `
+    Prediction: \n${result[0].className}\nindex
+    Probability: \n${result[0].probability}\n
+  `
+}
 
 async function app() {
-  console.log('Loading mobilenet..');
+  console.log('Loading mobilenet...');
 
-  // Load the model.
+  // Load the model.  
   net = await mobilenet.load();
   console.log('Sucessfully loaded model');
 
   // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result);
+  document.getElementById('btn-predict').addEventListener("click", classifyImg);
 }
 
 app();
